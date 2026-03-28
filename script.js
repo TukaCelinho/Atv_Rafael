e05cf0e512e4b33352436542772287951ab40295
Artur Celino <arturcelino@gmail.com>
2026-03-28 12:05:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const test = () => {
+    var home = document.getElementById
+    ("#home").innerText
+
+    if (home === "Home") {
+        document.getElementById
+        ("#home").innerText =
+        "Testando"
+    } else {
+        document.getElementById
+        ("#home").innerText = "Home"
+    }
+}
